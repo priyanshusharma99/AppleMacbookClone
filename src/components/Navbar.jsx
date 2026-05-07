c282a9a8d2515641074e300182372b11e4f58be4
Priyanshu <priyanshu.sharmaiia@gmail.com>
2026-05-07 11:37:25 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src="/logo.svg" alt="Apple Logo" />
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
