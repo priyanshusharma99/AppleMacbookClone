50df92cba6b84bdadee29e002f38b3a23dae8da3
Priyanshu <priyanshu.sharmaiia@gmail.com>
2026-05-08 20:22:24 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,26 @@ const Navbar = () => {
     <header>
         <nav>
             <img src="/logo.svg" alt="Apple Logo" />
+            <ul>
+                {[
+                    {label: 'store'},
+                    {label: 'mac'},
+                    {label: 'iPhone'},
+                    {label: 'watch'},
+                    {label: 'vision'},
+                    {label: 'airpods'},
+                ].map((item, index) => (
+                    <li key={index}><a href={`/${item.label.toLowerCase()}`}>{item.label}</a></li>
+                ))}
+            </ul>
+            <div className='flex-center gap-3'>
+                <button>
+                    <img src="/search.svg" alt="search" />
+                </button>
+                <button>
+                    <img src="/cart.svg" alt="cart" />
+                </button>
+            </div>
         </nav>
     </header>
   )
