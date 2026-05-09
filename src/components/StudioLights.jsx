5fc7b9f7ca3a4cd66a4bffca6b5665469ddbaabe
Priyanshu <priyanshu.sharmaiia@gmail.com>
2026-05-09 11:08:41 +0530

    First macbook-16 3d model added to canvas

---
+++ src/components/StudioLights.jsx
@@ -0,0 +1,48 @@
+import { Environment, Lightformer } from "@react-three/drei";
+import React from "react";
+import { Group } from "three/examples/jsm/libs/tween.module.js";
+
+const StudioLights = () => {
+  return (
+    <group>
+      <Environment resolution={256}>
+        <group>
+          <Lightformer
+            form="rect"
+            intensity={10}
+            position={[-10, 5, -5]}
+            scale={10}
+            rotation-y ={Math.PI / 2}
+          />
+          <Lightformer
+            form="rect"
+            intensity={10}
+            position={[10, 0, 1]}
+            scale={10}
+            rotation-y ={Math.PI / 2}
+          />
+        </group>
+      </Environment>
+      <spotLight
+        intensity={Math.PI*0.2}
+        angle={0.15}
+        decay={0}
+        position={[-2, 10, 5]}
+      /> 
+      <spotLight
+        intensity={Math.PI*0.2}
+        angle={0.15}
+        decay={0}
+        position={[0, -25, 10]}
+      /> 
+      <spotLight
+        intensity={Math.PI*1}
+        angle={0.15}
+        decay={0.1}
+        position={[0, 15, 5]}
+      /> 
+    </group>
+  );
+};
+
+export default StudioLights;
